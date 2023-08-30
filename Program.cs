cc9d863c8e1730cd08151e630b69a302bb5a3ee1
balogun14 <awwalbalogun06@gmail.com>
2023-08-30 14:15:30 +0100

    practiced formatted strings

--- Program.cs
+++ Program.cs
@@ -6,19 +6,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Mosh";
-            bool isWorking = true;
-
-            Console.WriteLine(number);
-            System.Console.WriteLine(count);
-            System.Console.WriteLine(totalPrice);
-            System.Console.WriteLine(character);
-            System.Console.WriteLine(firstName);
-            System.Console.WriteLine(isWorking);
+            System.Console.WriteLine("{0}-{1}", byte.MinValue, byte.MaxValue);
+            System.Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            System.Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
         }
     }
 }
