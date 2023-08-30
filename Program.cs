82c638ff3c6f5c199338c83698f7b95a6c1366b7
balogun14 <awwalbalogun06@gmail.com>
2023-08-30 14:14:40 +0100

    practiced dataset

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System;
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = true;
+
+            Console.WriteLine(number);
+            System.Console.WriteLine(count);
+            System.Console.WriteLine(totalPrice);
+            System.Console.WriteLine(character);
+            System.Console.WriteLine(firstName);
+            System.Console.WriteLine(isWorking);
         }
     }
 }
